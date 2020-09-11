36809e5729835fbb62fed4816f49e6b652aa2f5b
bridget462 <bridges.dev.create@gmail.com>
2020-09-11 22:23:28 +0900

    progress bar click now jump the song

--- script.js
+++ script.js
@@ -127,7 +127,19 @@ function updateProgressBar(e) {
   }
 }
 
+// Set Progress Bar
+function setProgressBar(e) {
+  const width = this.clientWidth;
+  const clickX = e.offsetX;
+  const { duration } = music;
+  console.log((clickX / width) * duration);
+
+  music.currentTime = (clickX / width) * duration;
+}
+
 // Event Listeners
 prevBtn.addEventListener("click", prevSong);
 nextBtn.addEventListener("click", nextSong);
+music.addEventListener("ended", nextSong);
 music.addEventListener("timeupdate", updateProgressBar);
+progressContainer.addEventListener("click", setProgressBar);
